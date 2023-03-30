534f9ffe9d8369aa4838be97e026e099dab63220
Mark J Menger <mjmenger@users.noreply.github.com>
2023-03-30 20:29:51 +0000

    fix:snake case for input

--- main.tf
+++ main.tf
@@ -9,23 +9,23 @@ terraform {
     aws = ">= 2.24"
   }
 }
-variable "projectPrefix" {
+variable "project_prefix" {
   type        = string
   description = "projectPrefix name for tagging"
 }
 resource "volterra_virtual_site" "virtual_site" {
-  name      = format("%s-site-group", var.projectPrefix)
+  name      = format("%s-site-group", var.project_prefix)
   namespace = "shared"
 
   site_selector {
-    expressions = [format("site-group in (%s)", var.projectPrefix)]
+    expressions = [format("site-group in (%s)", var.project_prefix)]
   }
 
   site_type = "CUSTOMER_EDGE"
 }
 
 resource "volterra_site_mesh_group" "site-group" {
-  name        = format("%s-site-group", var.projectPrefix)
+  name        = format("%s-site-group", var.project_prefix)
   namespace   = "system"
   tunnel_type = "SITE_TO_SITE_TUNNEL_IPSEC"
   type        = "SITE_MESH_GROUP_TYPE_FULL_MESH"
